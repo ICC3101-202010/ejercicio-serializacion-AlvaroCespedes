0bcc1c41db46ca63176bb4f9afbac41d2e7c24e1
AlvaroCespedes <aecespedes@miuandes.cl>
2020-05-07 10:10:13 -0400

    Finish

---
+++ Serializacion/Persona.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+namespace Serializacion
+{
+    [Serializable()]
+    public class Persona : ISerializable
+    {
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+        public Persona(string nombre, string apellido, int edad)
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._edad = edad;
+        }
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+        public int Edad
+        {
+            get { return _edad; }
+            set { _edad = value; }
+        }
+
+        // Es la funcion de la serializacion. Guardara la data in a file.
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            //Asignamos nuestros valores.
+            info.AddValue("Nombre", _nombre);
+            info.AddValue("Apellido", _apellido);
+            info.AddValue("edad", _edad);
+        }
+
+        //Deserializar
+        public Persona(SerializationInfo info, StreamingContext context)
+        {
+            _nombre = (string)info.GetValue("Nombre", typeof(string));
+            _apellido = (string)info.GetValue("Apellido", typeof(string));
+            _edad = (int)info.GetValue("Edad", typeof(int));
+        }
+
+        public Persona()
+        {
+        }
+    }
+}
--- Serializacion/Program.cs
+++ Serializacion/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.Collections.Immutable;
+
+
 
 namespace Serializacion
 {
@@ -6,7 +13,80 @@ namespace Serializacion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona persona = new Persona();
+            Stream stream = File.Open("PersonaData.dat", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            // I need to send the object data to my file.
+            //bf.Serialize(stream, persona);
+            //stream.Close();
+
+            //Now the data its saved.
+
+            //Read the object data from the file.
+            stream = File.Open("PersonaData.dat", FileMode.Open);
+            bf = new BinaryFormatter();
+            //persona = (Persona)bf.Deserialize(stream);
+            //stream.Close();
+
+            Console.WriteLine("Aprete 10 para ingresar al menu");
+            string opcion1 = Console.ReadLine();
+            while(opcion1 == "10") 
+            {
+                Console.WriteLine("(1) Crear persona. ");
+                Console.WriteLine("(2) Mostrar Personas. ");
+                Console.WriteLine("(3) Cargar Personas. Deserializar. ");
+                Console.WriteLine("(4) Guardar personas en un archivo. Serializar. ");
+                Console.WriteLine("0 Sali.");
+                string opcion2 = Console.ReadLine();
+                if (opcion2 == "1")
+                {
+                    Console.WriteLine("Nombre: ");
+                    string nombre = Console.ReadLine();
+
+                    Console.WriteLine("Apellido: ");
+                    string apellido = Console.ReadLine();
+
+                    Console.WriteLine("Edad: ");
+                    string edad = Console.ReadLine();
+                    int edad1 = Convert.ToInt32(edad);
+
+                    persona.Nombre = nombre;
+                    persona.Apellido = apellido;
+                    persona.Edad = edad1;
+
+                    Console.WriteLine("Se ha agregado correctamente");
+                    Console.WriteLine("Elegir nueva opcion. ");
+                    string opcion3 = Console.ReadLine();
+                    opcion2 = opcion3;
+                    break;
+
+                }
+                if (opcion2 == "2")
+                {
+                    //Mostrando todas las personas.
+                    Console.WriteLine(persona.ToString());
+                    string opcion3 = Console.ReadLine();
+                    opcion2 = opcion3;
+                }
+                if (opcion2 == "3")
+                {
+                    persona = (Persona)bf.Deserialize(stream);
+                    stream.Close();
+                    string opcion3 = Console.ReadLine();
+                    opcion2 = opcion3;
+                }
+                if (opcion2 == "4")
+                {
+                    bf.Serialize(stream, persona);
+                    stream.Close();
+                    string opcion3 = Console.ReadLine();
+                    opcion2 = opcion3;
+                }
+                if (opcion2 == "0")
+                {
+                    break;
+                }
+            }
         }
     }
 }
